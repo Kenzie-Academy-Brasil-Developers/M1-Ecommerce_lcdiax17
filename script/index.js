091aa4fc3be7a9e6569eeed642e57ced270eb947
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-03-14 15:27:42 -0300

    Subindo alterações realizadas, ainda não encontrei o erro de renderização

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 const mainCards = document.querySelector('main-cards')
-const mainUl = document.getElementsByTagName('ul')
+const mainUl = document.querySelector('main-ul')
 
 
 let productsData = [];
@@ -7,7 +7,7 @@ let productsData = [];
 function renderProductsData(list){
 
     for(let i = 0; i < list.length; i++){
-        if(list[i] == id[i]){
+        if(list[i].id > 0){
             productsData.push(list[i])
         }
     }
@@ -17,4 +17,19 @@ renderProductsData(data)
 
 for(let i = 0; i < productsData.length; i++){
     const listLi = document.createElement('li')
+    const imageLi = document.createElement('img')
+    const titleLi = document.createElement('h3')
+    const descriptionLi = document.createElement('p')
+    const valueLi = document.createElement('p')
+    const butttonLi = document.createElement('button')
+
+    imageLi.setAttribute('src', productsData[i].img)
+    titleLi.innerText = productsData[i].nameItem
+    descriptionLi.innerText = productsData[i].description
+    valueLi.innerText = productsData[i].value
+    butttonLi.innerHTML = productsData[i].addCart
+
+    listLi.append(imageLi, titleLi, descriptionLi, valueLi, butttonLi)
+
+    
 }
