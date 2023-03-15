09113630e4331a04f6a40e3607564fccaa779600
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-03-15 11:55:00 -0300

    Passei meu loop para dentro de uma função

--- script/index.js
+++ script/index.js
@@ -1,10 +1,9 @@
 const mainCards = document.querySelector('main-cards')
 const mainUl = document.querySelector('.main-ul')
 
-
 let productsData = [];
 
-function renderProductsData(list){
+function ProductsDataList(list){
 
     for(let i = 0; i < list.length; i++){
         if(list[i].id > 0){
@@ -12,34 +11,36 @@ function renderProductsData(list){
         }
     }
 }
+ProductsDataList(data)
 
-renderProductsData(data)
-
-for(let i = 0; i < productsData.length; i++){
-    const listLi = document.createElement('li')
-    const imageLi = document.createElement('img')
-    const tagLi = document.createElement('p')
-    const titleLi = document.createElement('h3')
-    const descriptionLi = document.createElement('p')
-    const valueLi = document.createElement('p')
-    const butttonLi = document.createElement('button')
-
-    listLi.className = 'list-class'
-    imageLi.className = 'image-li'
-    tagLi.classList = 'tag-li'
-    titleLi.className = 'title-li'
-    descriptionLi.className = 'description-li'
-    valueLi.className = 'value-li'
-    butttonLi.className = 'button-li'
-
-    imageLi.setAttribute('src', productsData[i].img)
-    tagLi.innerText = productsData[i].tag
-    titleLi.innerText = productsData[i].nameItem
-    descriptionLi.innerText = productsData[i].description
-    valueLi.innerText = productsData[i].value
-    butttonLi.innerHTML = productsData[i].addCart
-
-    listLi.append(imageLi, tagLi, titleLi, descriptionLi, valueLi, butttonLi)
+function renderAllProducts(list){
+    for(let i = 0; i < list.length; i++){
+        const listLi = document.createElement('li')
+        const imageLi = document.createElement('img')
+        const tagLi = document.createElement('p')
+        const titleLi = document.createElement('h3')
+        const descriptionLi = document.createElement('p')
+        const valueLi = document.createElement('p')
+        const butttonLi = document.createElement('button')
     
-    mainUl.appendChild(listLi)
+        listLi.className = 'list-class'
+        imageLi.className = 'image-li'
+        tagLi.classList = 'tag-li'
+        titleLi.className = 'title-li'
+        descriptionLi.className = 'description-li'
+        valueLi.className = 'value-li'
+        butttonLi.className = 'button-li'
+    
+        imageLi.setAttribute('src', list[i].img)
+        tagLi.innerText = list[i].tag
+        titleLi.innerText = list[i].nameItem
+        descriptionLi.innerText = list[i].description
+        valueLi.innerText = list[i].value
+        butttonLi.innerHTML = list[i].addCart
+    
+        listLi.append(imageLi, tagLi, titleLi, descriptionLi, valueLi, butttonLi)
+        
+        mainUl.appendChild(listLi)
+    }
 }
+renderAllProducts(data)
